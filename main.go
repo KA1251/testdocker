fa70e2ef4f4741e22f82fe2e20eabe74cec8236f
1251 <kir190477@mail.ru>
2024-03-08 15:14:20 +0300

    test2

--- main.go
+++ main.go
@@ -4,68 +4,63 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"time"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbHost  = "cockroachdb" // имя сервиса CockroachDB в сети Docker
-	dbPort  = 26257
-	dbUser  = "root"
-	dbName  = "test_db"
-	sslMode = "disable"
-)
-
 func main() {
-	// Формирование строки подключения к базе данных
-	connStr := fmt.Sprintf("postgresql://%s@%s:%d/%s?sslmode=%s", dbUser, dbHost, dbPort, dbName, sslMode)
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
 
-	// Подключение к базе данных
-	var db *sql.DB
-	var err error
-	for i := 0; i < 10; i++ { // Пробуем подключиться 10 раз с интервалом в 1 секунду
-		db, err = sql.Open("postgres", connStr)
-		if err != nil {
-			log.Printf("Error connecting to the database: %v. Retrying...", err)
-			time.Sleep(time.Second)
-		} else {
-			break
-		}
-	}
+	connStr := fmt.Sprintf("postgresql://%s@%s:%s/%s?sslmode=disable", dbUser, dbHost, dbPort, dbName)
 
-	if db == nil {
-		log.Fatal("Failed to connect to the database after multiple attempts")
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Error connecting to the database:", err)
 	}
 	defer db.Close()
 
-	// Создание таблицы
+	// Создаем базу данных, если ее еще не существует
+	if _, err := db.Exec(fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s", dbName)); err != nil {
+		log.Fatal("Error creating database:", err)
+	}
+	fmt.Println("created")
+	// Проверяем существует ли база данных
+	var exists bool
+	err = db.QueryRow(`SELECT EXISTS(SELECT 1 FROM information_schema.schemata WHERE schema_name = $1)`, dbName).Scan(&exists)
+	if err != nil {
+		log.Fatal("Error checking database existence:", err)
+	}
+	if !exists {
+		log.Fatalf("Database %s does not exist11", dbName)
+	}
+
 	if _, err := db.Exec(`CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name STRING)`); err != nil {
-		log.Fatal("Error creating table: ", err)
+		log.Fatal("Error creating table:", err)
 	}
 
-	// Вставка данных
 	if _, err := db.Exec(`INSERT INTO users (name) VALUES ($1)`, "John Doe"); err != nil {
-		log.Fatal("Error inserting data: ", err)
+		log.Fatal("Error inserting data:", err)
 	}
 
-	// Чтение данных
 	rows, err := db.Query(`SELECT id, name FROM users`)
 	if err != nil {
-		log.Fatal("Error querying data: ", err)
+		log.Fatal("Error querying data:", err)
 	}
 	defer rows.Close()
 
-	// Вывод данных
 	for rows.Next() {
 		var id int
 		var name string
 		if err := rows.Scan(&id, &name); err != nil {
-			log.Fatal("Error scanning row: ", err)
+			log.Fatal("Error scanning row:", err)
 		}
 		fmt.Printf("ID: %d, Name: %s\n", id, name)
 	}
 	if err := rows.Err(); err != nil {
-		log.Fatal("Error iterating rows: ", err)
+		log.Fatal("Error iterating rows:", err)
 	}
 }
